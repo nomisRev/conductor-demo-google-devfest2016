39c5fa643c65de2fa7685f7b1f719ac6ec89b77b
Simon Vergauwen <vergauwen.simon@gmail.com>
2016-11-27 14:59:01 +0100

    Fixed scopes

--- core/src/main/kotlin/be/vergauwen/simon/common/di/scopes/ActivityScope.kt
+++ core/src/main/kotlin/be/vergauwen/simon/common/di/scopes/ActivityScope.kt
@@ -3,6 +3,4 @@ package be.vergauwen.simon.common.di.scopes
 import javax.inject.Named
 
 //When using conductor Application/Activity scope is the same.
-@Named
-@Retention
-annotation class ActivityScope
+@scope annotation class ActivityScope
--- core/src/main/kotlin/be/vergauwen/simon/common/di/scopes/ViewScope.kt
+++ core/src/main/kotlin/be/vergauwen/simon/common/di/scopes/ViewScope.kt
@@ -2,6 +2,4 @@ package be.vergauwen.simon.common.di.scopes
 
 import javax.inject.Scope
 
-@Scope
-@Retention
-annotation class ViewScope
+@Scope annotation class ViewScope
